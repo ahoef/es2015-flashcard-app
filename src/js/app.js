64d3ba3d517a818c6e6916718f787bf125ec798f
Alexandra Hoefinger <alexandra.hoefinger@gmail.com>
2016-01-12 14:27:15 -0500

    add flashcard view file

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,6 @@
 import controller from "./flashcardController.js";
-import model from './flashCardModel.js';
-
-
-$('.next').on('click', () => {
-	const action = "next";
-	controller.render(model, action); 
-});
-
-$('.back').on('click', () => {
-	const action = "back";
-	controller.render(model, action); 
-});
-
-$('.show-answer').on('click', () => {
-	$('.answer').show(); 
-});
-
+import model from './flashcardModel.js';
+import view from './flashcardView.js';
 
 controller.render(model);
 
---
+++ src/js/flashcardView.js
@@ -0,0 +1,19 @@
+import controller from "./flashcardController.js";
+import model from './flashcardModel.js';
+
+
+$('.next').on('click', () => {
+	const action = "next";
+	controller.render(model, action); 
+});
+
+$('.back').on('click', () => {
+	const action = "back";
+	controller.render(model, action); 
+});
+
+$('.show-answer').on('click', () => {
+	$('.answer').show(); 
+});
+
+
